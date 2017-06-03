f4b29a954f00b526ba45a976536fbc1fb68b38b4
Cristian Garrido <crgarrido.s@gmail.com>
2017-06-03 09:49:08 +0200

    divised but not detecting some kotkin keywords

--- app/src/main/kotlin/com/tochange/bonjia/main/MainPresenter.kt
+++ app/src/main/kotlin/com/tochange/bonjia/main/MainPresenter.kt
@@ -1,7 +1,6 @@
 package com.tochange.bonjia.main
 
 import com.tochange.bonjia.base.BasePresenter
-import com.tochange.bonjia.entity.User
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -14,13 +13,13 @@ class MainPresenter : BasePresenter<IMainView>(){
     private var loadUsersSubscription: Disposable? = null
 
     fun loadUserList() {
-        val juan = User("juancho", "CL", listOf("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"))
-        val pepe = User("pepiton", "PE")
-        val cholo = User("cholomon", "BO")
+        val juan = User("Juanchito", "CL", listOf("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"))
+        val pepe = User("Pepiton", "PE")
+        val cholo = User("Cholomon", "BO")
 
         val juanPeruano = juan.copy(country = "PE")
         loadUsersSubscription = Observable.just(listOf(juan))//, listOf(pepe, cholo))
-                .delay(3, TimeUnit.SECONDS)
+                .delay(1, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ users ->
--- model/src/main/java/com/tochange/bonjia/pojo/User.kt
+++ model/src/main/java/com/tochange/bonjia/pojo/User.kt
@@ -3,4 +3,4 @@ package com.tochange.bonjia.pojo
 /**
  * Created by cristian on 30/05/17
  */
-data class User(val username: String, val country: String)
+data class User(val username: String, val country: String, val photos : List<String> = listOf())
