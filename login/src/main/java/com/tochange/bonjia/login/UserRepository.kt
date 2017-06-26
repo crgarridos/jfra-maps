dbabcdfdb806158b8d71a0eb30a0d6f6624057bf
Cristian GARRIDO <crgarrido.S@gmail.com>
2017-06-26 16:06:23 +0200

    masked firebase exception by internal ones

--- login/src/main/java/com/tochange/bonjia/login/UserRepository.kt
+++ login/src/main/java/com/tochange/bonjia/login/UserRepository.kt
@@ -1,32 +1,37 @@
 package com.tochange.bonjia.login
 
 import com.androidhuman.rxfirebase2.auth.RxFirebaseAuth
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.*
 import com.tochange.bonjia.model.User
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 
 /**
- * Created by cristian on 21/06/17.
+ * Created by cristian on 21/06/17
  */
 object UserRepository {
 
     private val mAuth by lazy { FirebaseAuth.getInstance() }
 
     fun createUser(email: String, password: String): Single<User> {
-        return RxFirebaseAuth.createUserWithEmailAndPassword(mAuth, email, password)
-                .map { it.parseUser() }
-                .doOnError { error ->
-                    throw when(error){
-                        is FirebaseAuthUserCollisionException ->  UserAlreadyExistsException()
-                        else -> UserUnknownException(error)
-                    }
-                }
+        return maskFirebaseUserSingle(RxFirebaseAuth.createUserWithEmailAndPassword(mAuth, email, password))
     }
+
     fun getUser(email: String, password: String): Single<User> {
-        return RxFirebaseAuth.signInWithEmailAndPassword(mAuth, email, password)
+        return maskFirebaseUserSingle(RxFirebaseAuth.signInWithEmailAndPassword(mAuth, email, password))
+    }
+
+    private fun maskFirebaseUserSingle(firebaseUserSingle: Single<FirebaseUser>): Single<User> {
+        return firebaseUserSingle.subscribeOn(Schedulers.io())
                 .map { it.parseUser() }
+                .onErrorResumeNext { error ->
+                    Single.error(when(error){
+                        is FirebaseAuthUserCollisionException ->  UserAlreadyExistsException(error)
+                        is FirebaseAuthInvalidCredentialsException ->  UserInvalidCredentialsException(error)
+                        is FirebaseAuthInvalidUserException ->  UserInvalidCredentialsException(error)
+                        else -> UserUnknownException(error)
+                    })
+                }
     }
 
     private fun FirebaseUser.parseUser() : User {
